8e4bd7526ee66a7fb878d774b11df516b39f77de
Rodrigo Tufiño <rtufino@ups.edu.ec>
2020-07-12 12:30:51 -0500

    Agregar archivo de python

--- get-data.php
+++ get-data.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * Proyecto: ECG IoT
+ * Autora: Catalina Andrade
+ * Versión: 1.0.0
+ * Fecha: 12-07-2020
+ */
 
 // Setear la zona horaria a Ecuador
 date_default_timezone_set('America/Guayaquil');
@@ -7,25 +13,28 @@ date_default_timezone_set('America/Guayaquil');
 $servidor = "localhost";    // Ip o dominio del servidor de BDD
 $usuario = "root";          // Nombre del usuario de BDD
 $password = "";             // Contraseña del usuario BDD
-$base_datos = "ecg";   // Nombre de la base de datos
+$base_datos = "ecg";   		// Nombre de la base de datos
 
 // Crear la conexión con la Base de datos específica
 $conn = new mysqli($servidor, $usuario, $password, $base_datos);
 
 // Verificar si la conexion es exitosa
 if ($conn->connect_error) {
-    // Si hay un error en la conexión
+    // Si hay un error en la conexión termina la apliación
     die("Conexión fallida: " . $conn->connect_error);
 }
 
 // Crear la sentencia de consulta SQL 
-$sentencia = "select * from senal order by id DESC limit 1000";
+$sentencia = "select * from senal order by id DESC limit 500";
+
 // Ejecutar la consulta y almacenar el resultado
 $resultado = $conn->query($sentencia);
+
 // Verificar si la consulta arrojó resultados
 if ($resultado->num_rows > 0) {
-    // recorrer todos los registros del resultado
+    // armar el resutado con formato CSV
     echo "Tiempo, Value\n";
+	// recorrer todos los registros del resultado
     while ($registro = $resultado->fetch_assoc()) {
         echo $registro["id"] . "," . $registro["valor"] . "\n"; 
     }
