25552b50d3d70279139a2a7791b67c48f83498c8
root <root@ip-172-31-5-233.us-east-2.compute.internal>
2020-07-30 16:23:31 -0500

    Ajustes de la gráfica

--- get-data.php
+++ get-data.php
@@ -5,8 +5,8 @@ date_default_timezone_set('America/Guayaquil');
 
 // Conexion con la BDD
 $servidor = "localhost";    // Ip o dominio del servidor de BDD
-$usuario = "root";          // Nombre del usuario de BDD
-$password = "";             // Contraseña del usuario BDD
+$usuario = "admindb";          // Nombre del usuario de BDD
+$password = "roY4l.tea";             // Contraseña del usuario BDD
 $base_datos = "ecg";   // Nombre de la base de datos
 
 // Crear la conexión con la Base de datos específica
@@ -19,7 +19,7 @@ if ($conn->connect_error) {
 }
 
 // Crear la sentencia de consulta SQL 
-$sentencia = "select * from senal order by id DESC limit 1000";
+$sentencia = "select * from senal order by id DESC limit 500";
 // Ejecutar la consulta y almacenar el resultado
 $resultado = $conn->query($sentencia);
 // Verificar si la consulta arrojó resultados
--- js/index.js
+++ js/index.js
@@ -3,10 +3,20 @@ Highcharts.chart('grafica-ecg', {
         type: 'spline'
     },
     title: {
-        text: 'Live Data ECG'
+        text: 'Sistema de Monitoreo constante de una señal ECG'
     },
+    yAxis: {
+title: {
+text: "Amplitud (mV)"
+}
+},
+xAxis: {
+title: {
+text: "Tiempo (ms)"
+}
+},
     data: {
-        csvURL: 'http://localhost/ecg/get-data.php',
+        csvURL: 'http://18.188.129.30/ecg/get-data.php',
         enablePolling: true,
         dataRefreshRate: 1
     }
